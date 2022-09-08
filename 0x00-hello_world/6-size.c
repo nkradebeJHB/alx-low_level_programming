298bab8125995fe206e1da0997db6670ed04cdc3
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-08 16:22:55 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a long int: %d byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %d byte(s)\n", (sizeof(ll));
 	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
