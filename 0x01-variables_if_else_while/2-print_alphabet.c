5834deb6f299014eea0da6b741886a99f838f4c0
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-10 13:25:33 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <- 'z')
+	{
+		putchar(c);
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
