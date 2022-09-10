a5494e09e88c249e42da44a8fe9668437b5e3a86
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-10 13:52:51 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Prints the alphabet except the letters q and e.
- *  (*
- *   * Return: 0 on success
- *    */
+ * main - Prints the alphabet except the letters q and e.
+ * 
+ * Return: 0 on success
+ */
 int main(void)
 {
 	char c = 'a';
