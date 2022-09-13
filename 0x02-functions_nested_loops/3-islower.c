ccf317f2385e8fb7d71d2dfa93c5786d7e5d7160
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-13 14:36:47 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * _islower - checks if a charecter c is lowercase
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return(0);
+}
