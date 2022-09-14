14e42274db99e5890bd7384b8abb25fa32dad6d8
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-13 17:38:02 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two input numbers
+ * @i: First number
+ * @j: Second number
+ * Return: thesum of two numbers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
