b735fda92c1b8a6d74dc60f8cadeb6f7bb36e9e6
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-15 19:48:47 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{	
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar( j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
