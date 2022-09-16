a2170ed7b48bed146e9cb63729ba694d5c0fbc51
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-15 20:20:04 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square using #'s on a terminal.
+ * @n: the width and length of the square
+ * Return: Nothing
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	if ( n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
