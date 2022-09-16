e0011ceb15c54709e9cb0bd29225d15b702d74f7
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-15 20:03:42 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - draws a a straight line 
+ * @n: the number of underscores to print to make the line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+		for (i = 0; i < n ; i++)
+			_putchar('_');
+	_putchar('\n');
+}
