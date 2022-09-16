b81e8ca742c1f2a811518f7a0c1c61c41c575ad7
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-15 19:21:38 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,11 @@
 
 /**
  * mul - multipies two integers
+ * @a: first integer to multiply
+ * @b: second integer to multiply
  * Return:The results of the multiplication
  */
 int mul(int a, int b)
+{
 	return (a * b);
+}
