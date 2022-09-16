dc528fcffc96003bce1d7e02af15cb747af8a0cd
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-15 20:57:50 -0700

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - prints
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
