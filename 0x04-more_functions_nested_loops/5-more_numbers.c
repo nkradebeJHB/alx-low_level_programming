b86217648ea9ba3d0799b2d4ea8ece5343b9cb2d
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-15 20:30:40 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,10 +7,11 @@
 void more_numbers(void)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (i < 10)
-	{	
+	{
+		j = 0;	
 		while (j <= 14)
 		{
 			if (j >= 10)
@@ -19,7 +20,6 @@ void more_numbers(void)
 			j++;
 		}
 		_putchar('\n');
-		j = 0;
 		i++;
 	}
 }
