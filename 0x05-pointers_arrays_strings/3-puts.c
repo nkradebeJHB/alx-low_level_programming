779706cb031574cc4997bff247937db4463ffd81
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-19 17:48:55 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+/**
+ * _puts - prints a string to stdout.
+ * @str: string to be printed.
+ */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(s);
+		s++;
+	}
+	_putchar('\n');
+}
