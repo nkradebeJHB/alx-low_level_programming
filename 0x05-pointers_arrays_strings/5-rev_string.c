c7f04985a9e25bfaac9841068a6b6373813cd9a4
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-19 18:32:02 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+/**
+ * rev_string - reverse a string
+ * @s: inout string to be reversed.
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char r[];
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		r[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[j] = r[i];
+		i--;
+		j++;
+	}
+	s[j++] = '\0';
