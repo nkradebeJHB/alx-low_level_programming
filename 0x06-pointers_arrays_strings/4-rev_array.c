640bba3e849451625f8c1705f6738e7b6b8108be
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-21 14:19:02 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+/**
+ * reverse_array - reverses the content of an array of integers.
+ * @a: the array to reverse
+ * @n: size of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+	int rev[1000];
+
+	i = j = 0;
+	while (a[i] < n)
+	{
+		rev[i] = a[i];
+		i++;
+	}
+	while (i >= 0)
+	{
+		a[i] = rev[j];
+		i--;
+		j++;
+	}
+}
