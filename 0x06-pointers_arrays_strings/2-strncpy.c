dfa65c1b87c8c78cec38c84b9f5dcf3d6dcc6980
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-21 13:31:13 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+/**
+ * _strncpy - copies a string up to n
+ * @dest: Pointer to the concatinated string
+ * @src: point to the string to be copied
+ * @n: Number of bytes to copy.
+ * Return: Pointer to the destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	dest[i++] = '\0';
+	return (dest);
+}
