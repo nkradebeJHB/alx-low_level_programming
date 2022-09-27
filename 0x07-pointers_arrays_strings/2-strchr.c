0051b48306e7eae9f0bf42c7f9c2db4be86f4920
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-26 23:49:56 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] == c; i++)
 	{
-		if (*s == c)
-			return (s);
-		else if (*(s + 1) == c)
-			return (s + 1)
-		s++;
+		if (s[i] == c)
+			return (s[i]);
+		else if (s[i + 1] == c)
+			return (s[i + 1]);
 	}
-	return (s + 1);
+	return ('\0');
 }
