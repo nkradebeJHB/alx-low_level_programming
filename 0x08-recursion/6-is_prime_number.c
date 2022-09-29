c3df457fbc850fd03253275da75f77bce62f6779
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-09-28 20:01:17 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+/**
+ * check_prime - checks that a number is a prime
+ * @n: number to be checked
+ * @i: iterator
+ * Return: 1 if n is a prime, else return 0 otherwise.
+ */
+int check_prime(int n, int i)
+{
+	if (n % i == 0 || n < 2)
+		return (0);
+	else if (n == i - 1)
+		return (1);
+	else if (i < n)
+		return (check_prime(n, i + 1));
+	return (1);
+}
+/**
+ * is_prime_number - checks if a number is a prime.
+ * @n: the munber to be checked
+ * Return: 1 if n is a prime, else return 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
