bf7beca1ceab63550c6c672d63940fc6fa712422
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-01 00:21:22 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
