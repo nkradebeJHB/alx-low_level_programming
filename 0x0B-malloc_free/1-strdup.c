a8b0a1c420f957faf1a6c25312e6bf89cedc79b5
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-05 10:23:23 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - copies a string into a new buffer
+ * @str: the string to copy
+ * Return: pointer to a new buffer
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int len, i;
+
+	if (str == NULL)
+		return (NULL);
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	len++;
+	if (len < 1)
+		return (NULL);
+	ptr = malloc(sizeof(char) * len));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+		ptr[i] = str[i];
+	ptr[i] = '\0';
+	return (ptr);
+}
