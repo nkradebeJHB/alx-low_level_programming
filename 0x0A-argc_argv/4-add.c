7d44c7fb1ee1c30b5eea7a00de79e4b9c7f22d61
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-04 23:48:52 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,10 +10,10 @@
  * @j: int as an iterator for argv[][]
  * Return: 0 on success, 1 on failure
  */
-int is_digit(int argc, char *argv[], int i, unsigned int j)
+int is_digit(int argc, char *argv[],int i, unsigned int j)
 {
 	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
+		for (j = 0; argv[i][j] != '\0' && j < strlen(argv[i]); j++)
 			if (isdigit(argv[i][j]) == 0)
 				return (1);
 	return (0);
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	int sum, i;
 
 	sum = 0;
-	if (is_digit(argc, 1, 0, argv) == 1)
+	if (is_digit(argc, argv, 1, 0) == 1)
 	{
 		printf("Input is not a digit\n");
 		return (1);
