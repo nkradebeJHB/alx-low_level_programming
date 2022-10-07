e51f90dec7d551d02b481cab1ca23081cfbb72e1
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-06 18:10:14 -0700

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: size of buffer
- * Return: pointer to buffer on success, 
+ * Return: pointer to buffer on success,
  * normal process termination with a status value of 98
  */
 void *malloc_checked(unsigned int b)
