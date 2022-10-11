a9a9f4861120ecd200aa6faa5f7347ffd6374a4e
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-10 19:51:54 -0700

    changed dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-typedef struct dog dog_t;
 #endif
