eead439979f6013e51ef6388fa7d6ca055e74764
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-14 14:12:00 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (size <= 0)
 		return;
 
-	if (array == 0 || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	ptr = action;
-
 	for (i = 0; i < size; i++)
 		ptr(array[i]);
 }
