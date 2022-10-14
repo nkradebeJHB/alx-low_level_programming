b92849aead6f4f15762984b191ea3c9508540374
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-14 14:47:51 -0700

    1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer.
+ * @array: The array being passed.
+ * @size: The number of indexes in the array.
+ * @cmp: The pointer to a function.
+ * Return: The index of the first element,
+ * If no element matches, return -1 and If size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+	int (*ptr)(int);
+
+	if (size <= 0)
+		return (-1);
+
+	if (array == NULL || cmp == NULL)
+		retun (-1);
+	ptr = cmp;
+	for (i = 0; i < size; i++)
+	{
+		if (ptr(array[i]))
+			return (i);
+	}
+	return (-1);
+}	
