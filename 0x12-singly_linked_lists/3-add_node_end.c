29c5a6c5f8fb568b535ba01b530c72c11000d5e6
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-21 17:09:59 -0700

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,4 +7,33 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *address, l_address;
+	int len;
+	char *copy;
+
+	if (!head)
+		return (NULL);
+	address = malloc(sizeof(list_t));
+
+	if (address == NULL)
+		return (NULL);
+	copy = strdup(str);
+
+	for (len = 0; str[len];)
+		len++;
+
+	address->str = copy;
+	address->len = len;
+	address->next = NULL;
+
+	if (!*head)
+		*head = address;
+	else
+	{
+		l_address = *head;
+		while (l_address->next != NULL)
+			l_address = l_address->next;
+		l_address->next = address;
+	}
+	return (*head);
 }
