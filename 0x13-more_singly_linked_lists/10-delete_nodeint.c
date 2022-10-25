eb3770faf80e93f1cdb36eec6ba515795ded7083
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-25 16:10:52 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(curlistint);
-		return (-1);
+		return (1);
 	}
 	for (i = 0; i < (index - 1); i++)
 	{
