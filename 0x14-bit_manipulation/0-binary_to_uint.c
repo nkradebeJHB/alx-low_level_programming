e46107fd74fbdef8e4142cef31e5ec68c1a5cbba
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-10-27 12:50:35 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,13 +15,14 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[str_len] == '0' || b[str_len] == '1')
 			str_len++;
+		else
+			return (0);
 	}
 
-	while (str_len)
+	for (str_len -= 1; str_len >= 0; str_len--, base *= 2)
 	{
-		sum += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
+		if (b[str_len] == '1')
+			sum += 1 * base;
 	}
 	return (sum);
 }
