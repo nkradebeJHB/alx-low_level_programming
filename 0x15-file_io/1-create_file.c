47eacff443b61abc9cd176abfd09fc497757b213
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-11-04 09:49:58 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	openfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0660);
+	openfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	writefile = write(openfile, text_content, len);
 
 	if (openfile == -1 || writefile == -1)
