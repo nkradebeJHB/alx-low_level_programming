26b84c4b82dd1ff895459ed68b5b7375df6afa64
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-11-04 12:02:46 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer = NULL;
 	ssize_t readfile, openfile, writefile;
-	
+
 	if (filename == NULL)
 		return (0);
 
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (openfile == -1)
 		return (0);
 
-	buffer =malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
