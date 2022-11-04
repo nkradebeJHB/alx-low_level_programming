dc8a2ed6940619c9be15cf493eb73ca8c9cf12c8
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-11-04 09:43:43 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ * Return: if success returns 1, else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int openfile, writefile, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	opennfile = open(filename, O_CREATE | O_RDWR | OTRUNC, 0660);
+	writefile = write(openfile, text_content, len);
+
+	if (openfile == -1 || writefile == -1)
+		return (-1);
+
+	close(openfile);
+	return (1);
+}
