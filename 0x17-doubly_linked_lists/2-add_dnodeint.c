fcff4bf492c57b2cc317bacaf8704a14dea78984
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2022-12-08 10:54:50 -0800

    2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a nes node at the beginning of a list
+ * @head: point to the beginning of the list
+ * @n: value to add into the list
+ * Return: pointer to the new element, or NULL if not successful
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->prev = NULL;
+	node->next = *head;
+	if(*head)
+		(*head)->prev = node;
+	*head = node;
+
+	return (node);
+}
