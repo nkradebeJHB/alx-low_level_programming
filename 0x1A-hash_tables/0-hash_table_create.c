38b1c2431a77fa291afdde523f571ab8c30900a4
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-08 13:50:46 -0800

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	node->size = size;
-	node->array = malloc(sizeof(hash_node_t **) * size);
+	node->array = malloc(sizeof(hash_node_t *) * size);
 	if (!node->array)
 	{
 		free(node);
