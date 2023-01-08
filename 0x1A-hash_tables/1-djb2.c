e216d7b256bccd9c53589a9d6ff7cbcc6e8eb3ce
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-08 14:12:17 -0800

    1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implements the djb algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int h;
+	int c;
+
+	h = 52381;
+	while((c = *str++))
+	{
+		h = ((h << 5) + h)) + c;
+	}
+	return (h);
+}
