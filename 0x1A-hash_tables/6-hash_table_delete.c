5b6ee627e5b5e5fe447eb3c22b0378a108469243
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-12 20:48:32 -0800

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *swap;
+	hash table_t *table_h = ht;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				swap = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = swap
+			}
+		}
+	}
+	free(table_h->array);
+	free(table_h);
+}
