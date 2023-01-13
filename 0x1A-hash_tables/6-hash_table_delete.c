682363babba219af9f43ae788b2f9ccced9a1c7a
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-12 20:56:44 -0800

    fixing 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,7 +8,7 @@ void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *node, *swap;
-	hash table_t *table_h = ht;
+	hash_table_t *table_h = ht;
 
 	for (i = 0; i < ht->size; i++)
 	{
