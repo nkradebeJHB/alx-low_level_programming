e982e31a71284afbed1b59aa37c9d09d916dffe0
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-12 20:34:08 -0800

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print -  prints a hash table.
+ * @ht: pointer to the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node;
+	char space = "";
+
+	if (!ht || !ht->array)
+		return;
+
+	putchar('{');
+	while (i < ht->size)
+	{
+		node = ((ht->array)[i]);
+		while (node)
+		{
+			printf("%s'%s': '%s'", space, node->key, node->value);
+			space = ", ";
+			node = node->next;
+		}
+		i++;
+	}
+	putchar("}");
+}
