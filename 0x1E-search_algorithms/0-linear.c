4a70f9b1cbe8bbf6f77fd61b8a850af8b4c52bd6
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-04-13 13:10:09 -0700

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,9 +18,9 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (size_i = 0; size_i < size; size_i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Value checked array[%ld] = [%d]\n", size_i, array[size_i]);
+		if (array[size_i] == value)
+			return (size_i);
 	}
 	return (-1);
 }
